f19ea3f0eb7014b2af87b64f81e73948bd2303ef
valrus <imccowan@gmail.com>
2021-03-21 21:06:04 -0700

    switch to mutagen
    ...no c dependencies

--- bespokeoke/karaokeize/tasks/lyrics.py
+++ bespokeoke/karaokeize/tasks/lyrics.py
@@ -19,7 +19,7 @@ SKIPPABLE_LINE_RE = re.compile(r'^[\[(].*?[\])]$')
 
 LYRICS_TAG_KEYS = {
     'MP4': '\xa9lyr',
-    'MP3': "USLT"
+    'MP3': 'USLT'
 }
 
 @make_task
@@ -31,9 +31,8 @@ def task_download_lyrics(input_path, output_path):
     def download_lyrics(targets):
         out_filename = targets[0]
         output_path.mkdir(parents=True, exist_ok=True)
-        mp3 = mutagen.File(str(input_path))
-
-        tag_lyrics = mp3.tags.get(self.lyrics_key())
+        audiofile = mutagen.File(str(input_path))
+        tag_lyrics = audiofile.tags.get(LYRICS_TAG_KEYS[audiofile.__class__.__name__])
 
         if tag_lyrics:
             with open(out_filename, 'w', encoding='utf-8') as lyrics_file:
@@ -46,11 +45,12 @@ def task_download_lyrics(input_path, output_path):
                     sep='\n', file=lyrics_file
                 )
         elif GENIUS_ACCESS_TOKEN:
+            audiofile_ez = mutagen.File(str(input_path), easy=True)
             genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
             # Remove section headers (e.g. [Chorus]) from lyrics when searching
             genius.remove_section_headers = True
             # TODO: Remove other annotation-y stuff as well
-            song = genius.search_song(mp3.tags['TITLE'], mp3.tags['ARTIST'])
+            song = genius.search_song(audiofile_ez['title'][0], audiofile_ez['artist'][0])
             with open(out_filename, 'w', encoding='utf-8') as lyrics_file:
                 print(song.lyrics, file=lyrics_file)
         else:
